68c4b56808a7d1f3e1fd326af08d50ef98b43868
kjk7212 <kjk7212@gmail.com>
2024-04-16 17:43:59 +0900

    test: Handlebars 리스트 사용 테스트

--- src/test/java/utils/HandlebarsTest.java
+++ src/test/java/utils/HandlebarsTest.java
@@ -1,10 +1,16 @@
 package utils;
 
+import java.util.Collection;
+
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Template;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
+
+import db.DataBase;
 import model.User;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +19,7 @@ public class HandlebarsTest {
     private static final Logger log = LoggerFactory.getLogger(HandlebarsTest.class);
 
     @Test
+    @DisplayName("동적 프로파일")
     void name() throws Exception {
         TemplateLoader loader = new ClassPathTemplateLoader();
         loader.setPrefix("/templates");
@@ -25,4 +32,25 @@ public class HandlebarsTest {
         String profilePage = template.apply(user);
         log.debug("ProfilePage : {}", profilePage);
     }
+
+
+    @Test
+    @DisplayName("동적 리스트")
+    void list() throws Exception {
+        TemplateLoader loader = new ClassPathTemplateLoader();
+        loader.setPrefix("/templates");
+        loader.setSuffix(".html");
+        Handlebars handlebars = new Handlebars(loader);
+
+        Template template = handlebars.compile("user/list");
+
+        User user = new User("cu", "password", "이동규", "brainbackdoor@gmail.com");
+        DataBase.addUser(user);
+        User user1 = new User("tset", "tes", "tse", "ste");
+        DataBase.addUser(user1);
+        Collection<User> users = DataBase.findAll();
+        String profilePage = template.apply(users);
+        log.debug("ProfilePage : {}", profilePage);
+    }
+
 }
