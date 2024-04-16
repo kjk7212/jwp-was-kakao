dc8b5f8f4f0d74e282a9978b9743c9ba787d2be7
kjk7212 <kjk7212@gmail.com>
2024-04-16 17:54:35 +0900

    test: body의 key / value 페어가 이상할 경우의 테스트 케이스 추가

--- src/main/java/parser/FormUrlEncodedParsingStrategy.java
+++ src/main/java/parser/FormUrlEncodedParsingStrategy.java
@@ -13,6 +13,7 @@ import http.HttpRequestBody;
 public class FormUrlEncodedParsingStrategy implements HttpBodyParsingStrategy {
 	private static final String PARAMETER_EQUAL_SIGN = "=";
 
+	//TODO : 주소에 []로 직접접근하기 때문에 쌍이 모자랄때 => key & value 페어가 맞춰지지 않을때는 확실히 에러를 뱉는데, 그 외에도 다른 검증 로직 추가를 검토해보자.
 	@Override
 	public HttpRequestBody parse(String body) {
 		if (body.isBlank()) {
